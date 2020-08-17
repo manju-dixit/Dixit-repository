3995c5838044cc1518c74da0c6e19fc5da2cc627
manju-dixit <61465813+manju-dixit@users.noreply.github.com>
2020-08-17 15:02:38 +0530

    Create script.js

---
+++ Responsive web design Projects/Personal Portfolio Webpage/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Variable declarations
+  var nav = document.getElementById("navbar");
+  var navUL = document.getElementById("nav");
+
+  // Responsive menu toggle
+
+  var toggle = document.getElementById("toggle");
+  toggle.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    nav.classList.toggle("is-open");
+
+  });
+
+  // Hide menu on click in body
+  document.getElementById("container").addEventListener("click", function(event) {
+    if (nav.classList.contains("is-open")) {
+      nav.classList.remove("is-open");
+    }
+  });
+
+  document.getElementById("navbar").addEventListener("click", function(event) {
+    if (nav.classList.contains("is-open")) {
+      nav.classList.remove("is-open");
+    }
+  });
+
+  /*document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});*/
+
+});
